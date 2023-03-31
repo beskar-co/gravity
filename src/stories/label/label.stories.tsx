37dfbbce77bfb70e95cd55cf9745ea9cebf187d1
Hayden Bleasel <hello@haydenbleasel.com>
2023-03-31 09:53:43 -0400

    Scaffold Label component

---
+++ src/stories/label/label.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Label } from './';
+
+export default {
+  title: 'Example/Label',
+  component: Label,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Label>;
+
+const Template: ComponentStory<typeof Label> = (args) => <Label {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Label',
+};
