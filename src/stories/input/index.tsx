0b0443744e191d2f7a636ee2888b09526e0ef14a
Hayden Bleasel <hello@haydenbleasel.com>
2023-03-31 09:56:29 -0400

    Wire up label props to input and textarea

--- src/stories/input/index.tsx
+++ src/stories/input/index.tsx
@@ -1,20 +1,31 @@
 import clsx from 'clsx';
 import type { InputHTMLAttributes } from 'react';
-import { forwardRef } from 'react';
+import { useId, forwardRef } from 'react';
+import { Label } from '../label';
 
-export type InputProps = InputHTMLAttributes<HTMLInputElement>;
+export type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+};
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, ...props }, ref) => (
-    <input
-      className={clsx(
-        'flex h-10 w-full rounded-md border border-neutral-300 bg-transparent px-3 py-2 text-sm placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-700 dark:text-neutral-50 dark:focus:ring-neutral-400 dark:focus:ring-offset-neutral-900',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
+  ({ className, ...props }, ref) => {
+    const id = useId();
+
+    return (
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        {props.label && <Label htmlFor={id}>{props.label}</Label>}
+        <input
+          id={id}
+          className={clsx(
+            'flex h-10 w-full rounded-md border border-neutral-300 bg-transparent px-3 py-2 text-sm placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-700 dark:text-neutral-50 dark:focus:ring-neutral-400 dark:focus:ring-offset-neutral-900',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  }
 );
 Input.displayName = 'Input';
 
--- src/stories/textarea/index.tsx
+++ src/stories/textarea/index.tsx
@@ -1,20 +1,30 @@
 import clsx from 'clsx';
 import type { TextareaHTMLAttributes } from 'react';
-import { forwardRef } from 'react';
+import { useId, forwardRef } from 'react';
+import { Label } from '../label';
 
-export type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
+export type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  label?: string;
+};
 
 const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => (
-    <textarea
-      className={clsx(
-        'flex h-20 w-full rounded-md border border-neutral-300 bg-transparent px-3 py-2 text-sm placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-700 dark:text-neutral-50 dark:focus:ring-neutral-400 dark:focus:ring-offset-neutral-900',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
+  ({ className, ...props }, ref) => {
+    const id = useId();
+
+    return (
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        {props.label && <Label htmlFor={id}>{props.label}</Label>}
+        <textarea
+          className={clsx(
+            'flex h-20 w-full rounded-md border border-neutral-300 bg-transparent px-3 py-2 text-sm placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-700 dark:text-neutral-50 dark:focus:ring-neutral-400 dark:focus:ring-offset-neutral-900',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  }
 );
 Textarea.displayName = 'Textarea';
 
