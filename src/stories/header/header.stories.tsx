000d4dbee26071956b8a90b65d0d5cc1c7403589
Hayden Bleasel <hello@haydenbleasel.com>
2023-03-30 22:16:53 -0400

    Start scaffolding header

---
+++ src/stories/header/header.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Header from './';
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
