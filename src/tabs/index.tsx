6cd6411126efa18dc4dd9b610785b943eeaf8e78
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-01 18:15:06 -0400

    Redesign tabs

--- src/tabs/index.tsx
+++ src/tabs/index.tsx
@@ -61,27 +61,34 @@ const Tabs = React.forwardRef<
       ref={ref}
       className={clsx('hidden sm:block', className)}
     >
-      <TabsPrimitive.List className="inline-flex items-center justify-center rounded-md bg-neutral-100 p-1 dark:bg-neutral-800">
+      <TabsPrimitive.List
+        className={clsx(
+          '-mb-px flex gap-4 border-b md:gap-6',
+          'border-neutral-100 dark:border-neutral-800'
+        )}
+      >
         {items.map((item) => (
           <TabsPrimitive.Trigger
             key={item.value}
             value={item.value}
             className={clsx(
-              'inline-flex min-w-[100px] items-center justify-center rounded-[0.185rem] px-3 py-1.5 text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm',
-              'text-neutral-700 data-[state=active]:bg-white data-[state=active]:text-neutral-900',
-              'dark:text-neutral-200 dark:data-[state=active]:bg-neutral-900 dark:data-[state=active]:text-neutral-100'
+              'flex whitespace-nowrap border-b-2 border-transparent px-1 py-4 text-sm font-medium',
+              'text-neutral-500 hover:border-neutral-700 hover:text-neutral-600',
+              'dark:text-neutral-400 dark:hover:border-neutral-200 dark:hover:text-neutral-300',
+              'data-[state=active]:border-neutral-900 data-[state=active]:text-neutral-900',
+              'dark:data-[state=active]:border-white dark:data-[state=active]:text-white'
             )}
             asChild={!!item.href}
           >
             {item.href ? (
-              <Link href={item.href} className="flex items-center gap-2">
+              <Link href={item.href}>
                 {item.label}
                 {item.badge && (
                   <Badge active={item.value === value}>{item.badge}</Badge>
                 )}
               </Link>
             ) : (
-              <span className="flex items-center gap-2">
+              <span>
                 {item.label}
                 {item.badge && (
                   <Badge active={item.value === value}>{item.badge}</Badge>
@@ -94,10 +101,7 @@ const Tabs = React.forwardRef<
       {items.some((item) => !item.href) &&
         value &&
         items.find((item) => item.value === value)?.content && (
-          <TabsPrimitive.Content
-            value={value}
-            className="mt-2 rounded-md border border-neutral-200 p-6 dark:border-neutral-700"
-          >
+          <TabsPrimitive.Content value={value} className="mt-4">
             {items.find((item) => item.value === value)?.content}
           </TabsPrimitive.Content>
         )}
