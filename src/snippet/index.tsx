1a93895f2ba6ca7287ef050423c375f5f161f0bc
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-01 13:55:51 -0400

    Add Snippet component

---
+++ src/snippet/index.tsx
@@ -0,0 +1,258 @@
+import type { FC } from 'react';
+import dynamic from 'next/dynamic';
+import { nord } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import { ClipboardDocumentIcon } from '@heroicons/react/24/outline';
+import { Tooltip } from '..';
+
+type HljsLanguage =
+  | '1c'
+  | 'abnf'
+  | 'accesslog'
+  | 'actionscript'
+  | 'ada'
+  | 'angelscript'
+  | 'apache'
+  | 'applescript'
+  | 'arcade'
+  | 'arduino'
+  | 'armasm'
+  | 'asciidoc'
+  | 'aspectj'
+  | 'autohotkey'
+  | 'autoit'
+  | 'avrasm'
+  | 'awk'
+  | 'axapta'
+  | 'bash'
+  | 'basic'
+  | 'bnf'
+  | 'brainfuck'
+  | 'c-like'
+  | 'c'
+  | 'cal'
+  | 'capnproto'
+  | 'ceylon'
+  | 'clean'
+  | 'clojure-repl'
+  | 'clojure'
+  | 'cmake'
+  | 'coffeescript'
+  | 'coq'
+  | 'cos'
+  | 'cpp'
+  | 'crmsh'
+  | 'crystal'
+  | 'cs'
+  | 'csharp'
+  | 'csp'
+  | 'css'
+  | 'd'
+  | 'dart'
+  | 'delphi'
+  | 'diff'
+  | 'django'
+  | 'dns'
+  | 'dockerfile'
+  | 'dos'
+  | 'dsconfig'
+  | 'dts'
+  | 'dust'
+  | 'ebnf'
+  | 'elixir'
+  | 'elm'
+  | 'erb'
+  | 'erlang-repl'
+  | 'erlang'
+  | 'excel'
+  | 'fix'
+  | 'flix'
+  | 'fortran'
+  | 'fsharp'
+  | 'gams'
+  | 'gauss'
+  | 'gcode'
+  | 'gherkin'
+  | 'glsl'
+  | 'gml'
+  | 'go'
+  | 'golo'
+  | 'gradle'
+  | 'groovy'
+  | 'haml'
+  | 'handlebars'
+  | 'haskell'
+  | 'haxe'
+  | 'hsp'
+  | 'htmlbars'
+  | 'http'
+  | 'hy'
+  | 'index'
+  | 'inform7'
+  | 'ini'
+  | 'irpf90'
+  | 'isbl'
+  | 'java'
+  | 'javascript'
+  | 'jboss-cli'
+  | 'json'
+  | 'julia-repl'
+  | 'julia'
+  | 'kotlin'
+  | 'lasso'
+  | 'latex'
+  | 'ldif'
+  | 'leaf'
+  | 'less'
+  | 'lisp'
+  | 'livecodeserver'
+  | 'livescript'
+  | 'llvm'
+  | 'lsl'
+  | 'lua'
+  | 'makefile'
+  | 'markdown'
+  | 'mathematica'
+  | 'matlab'
+  | 'maxima'
+  | 'mel'
+  | 'mercury'
+  | 'mipsasm'
+  | 'mizar'
+  | 'mojolicious'
+  | 'monkey'
+  | 'moonscript'
+  | 'n1ql'
+  | 'nginx'
+  | 'nim'
+  | 'nimrod'
+  | 'nix'
+  | 'node-repl'
+  | 'nsis'
+  | 'objectivec'
+  | 'ocaml'
+  | 'openscad'
+  | 'oxygene'
+  | 'parser3'
+  | 'perl'
+  | 'pf'
+  | 'pgsql'
+  | 'php-template'
+  | 'php'
+  | 'plaintext'
+  | 'pony'
+  | 'powershell'
+  | 'processing'
+  | 'profile'
+  | 'prolog'
+  | 'properties'
+  | 'protobuf'
+  | 'puppet'
+  | 'purebasic'
+  | 'python-repl'
+  | 'python'
+  | 'q'
+  | 'qml'
+  | 'r'
+  | 'reasonml'
+  | 'rib'
+  | 'roboconf'
+  | 'routeros'
+  | 'rsl'
+  | 'ruby'
+  | 'ruleslanguage'
+  | 'rust'
+  | 'sas'
+  | 'scala'
+  | 'scheme'
+  | 'scilab'
+  | 'scss'
+  | 'shell'
+  | 'smali'
+  | 'smalltalk'
+  | 'sml'
+  | 'sqf'
+  | 'sql'
+  | 'sql_more'
+  | 'stan'
+  | 'stata'
+  | 'step21'
+  | 'stylus'
+  | 'subunit'
+  | 'supported-languages'
+  | 'swift'
+  | 'taggerscript'
+  | 'tap'
+  | 'tcl'
+  | 'tex'
+  | 'thrift'
+  | 'tp'
+  | 'twig'
+  | 'typescript'
+  | 'vala'
+  | 'vbnet'
+  | 'vbscript-html'
+  | 'vbscript'
+  | 'verilog'
+  | 'vhdl'
+  | 'vim'
+  | 'x86asm'
+  | 'xl'
+  | 'xml'
+  | 'xquery'
+  | 'yaml'
+  | 'zephir';
+
+export const Snippet: FC<{
+  language: HljsLanguage;
+  code: string;
+  onCopySuccess?: (text: string) => void;
+  onCopyError?: (text: string) => void;
+}> = ({ language, code, onCopySuccess, onCopyError }) => {
+  const SyntaxHighlighter = dynamic(
+    async () =>
+      import(
+        /* webpackChunkName: "react-syntax-highlighter" */
+        'react-syntax-highlighter'
+      ),
+    { ssr: false }
+  );
+
+  const handleCopy = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+
+      onCopySuccess?.('Copied code to clipboard');
+    } catch (error) {
+      onCopyError?.('Failed to copy code to clipboard');
+    }
+  };
+
+  return (
+    <div className="relative overflow-hidden rounded border border-neutral-800 bg-black p-4">
+      <SyntaxHighlighter
+        language={language}
+        style={nord}
+        customStyle={{
+          background: 'transparent',
+          padding: 0,
+          fontSize: 14,
+        }}
+      >
+        {code}
+      </SyntaxHighlighter>
+      <Tooltip content="Copy to clipboard">
+        <button
+          type="button"
+          className="absolute right-4 top-4 rounded bg-neutral-900 p-2 transition-colors hover:bg-neutral-800"
+          onClick={async () => handleCopy(code)}
+        >
+          <ClipboardDocumentIcon
+            className="h-4 w-4 text-neutral-400"
+            width={16}
+            height={16}
+          />
+        </button>
+      </Tooltip>
+    </div>
+  );
+};
---
+++ src/snippet/snippet.stories.tsx
@@ -0,0 +1,61 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Snippet } from './';
+import { TooltipProvider } from '..';
+
+export default {
+  title: 'Example/Snippet',
+  component: Snippet,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Snippet>;
+
+const Template: ComponentStory<typeof Snippet> = (args) => (
+  <TooltipProvider>
+    <Snippet {...args} />
+  </TooltipProvider>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  language: 'javascript',
+  // eslint-disable-next-line no-console
+  onCopyError: console.log,
+  // eslint-disable-next-line no-console
+  onCopySuccess: console.log,
+  code: `const waitlist = 'cleftordg0002l8082tu040gj';
+const email = 'janesmith@acme.com';
+const name = 'Jane Smith'; // Optional
+const phone = '555-555-5555'; // Optional
+const referredBy = 'abc123'; // Optional
+const metadata = { userId: 'abcd' }; // Optional (Pro only)
+
+/* With client-side JavaScript, you don't need the API Key
+ * if you call the endpoint from a whitelisted domain. This
+ * way, you don't need to expose your API Key to the public.
+ */
+const response = await fetch('/api/subscribers/create', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    waitlist,
+    email,
+    name,
+    phone,
+    referredBy,
+    metadata,
+  }),
+});
+
+const body = (await response.json()) as { message: string };
+
+if (!response.ok) {
+  window.alert(body.message);
+  return;
+}
+
+window.alert('You have been subscribed!');`,
+};
