c261fe7be711471c59fae60b7e07ecf481bd37f6
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-02 16:13:06 -0400

    Attempt to rework package.json

--- scripts/generate-exports.js
+++ scripts/generate-exports.js
@@ -3,22 +3,37 @@ const path = require('path');
 
 const distFolder = 'dist';
 const packagePath = path.join(__dirname, '../package.json');
+const newPackagePath = path.join(__dirname, '../dist/package.json');
 const packageJSON = require(packagePath);
 
-function generateExports() {
-  const exports = {};
+const newPackageJSON = {
+  name: packageJSON.name,
+  description: packageJSON.description,
+  version: packageJSON.version,
+  author: packageJSON.author,
+  repository: packageJSON.repository,
+  license: packageJSON.license,
+  dependencies: packageJSON.dependencies,
+  peerDependencies: packageJSON.peerDependencies,
+  type: 'module',
+  files: ['*'],
+  sideEffects: false,
+  exports: fs.readdirSync(distFolder).map((dir) => ({
+    [`./${dir}`]: {
+      import: `./${dir}/index.mjs`,
+      require: `./${dir}/index.js`,
+      types: `./${dir}/index.d.ts`,
+    },
+  })),
+};
 
-  fs.readdirSync(distFolder).forEach((dir) => {
-    exports[`./${dir}`] = {
-      import: `./dist/${dir}/index.mjs`,
-      require: `./dist/${dir}/index.js`,
-      types: `./dist/${dir}/index.d.ts`,
-    };
-  });
+fs.writeFileSync(newPackagePath, JSON.stringify(newPackageJSON, null, 2));
 
-  return exports;
-}
+/* Copy static files */
+const staticFiles = ['CHANGELOG.md', 'README.md', 'yarn.lock'];
+staticFiles.forEach((file) => {
+  const source = path.resolve(file);
+  const target = path.resolve(`../dist/${file}`);
 
-packageJSON.exports = generateExports();
-
-fs.writeFileSync(packagePath, JSON.stringify(packageJSON, null, 2));
+  fs.copyFileSync(source, target);
+});
