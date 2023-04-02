491cf4b5dec7e84875eb887ceda10ae0e099bde8
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-02 11:48:48 -0400

    Fix colors

--- src/navigationMenu/index.tsx
+++ src/navigationMenu/index.tsx
@@ -79,12 +79,12 @@ export const NavigationMenu = forwardRef<
                       >
                         <Link
                           href={subItem.href}
-                          className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-slate-100 focus:bg-slate-100 dark:hover:bg-slate-700 dark:focus:bg-slate-700"
+                          className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
                         >
                           <div className="text-sm font-medium leading-none">
                             {subItem.label}
                           </div>
-                          <p className="line-clamp-2 text-sm leading-snug text-slate-500 dark:text-slate-400">
+                          <p className="line-clamp-2 text-sm leading-snug text-neutral-500 dark:text-neutral-400">
                             {subItem.description}
                           </p>
                         </Link>
