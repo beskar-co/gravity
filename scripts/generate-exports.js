7e25ff62d28f063924cd21869dab175e53b94004
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-02 16:28:15 -0400

    Revert "Attempt to rework package.json"
    This reverts commit c261fe7be711471c59fae60b7e07ecf481bd37f6.

--- scripts/generate-exports.js
+++ scripts/generate-exports.js
@@ -3,37 +3,22 @@ const path = require('path');
 
 const distFolder = 'dist';
 const packagePath = path.join(__dirname, '../package.json');
-const newPackagePath = path.join(__dirname, '../dist/package.json');
 const packageJSON = require(packagePath);
 
-const newPackageJSON = {
-  name: packageJSON.name,
-  description: packageJSON.description,
-  version: packageJSON.version,
-  author: packageJSON.author,
-  repository: packageJSON.repository,
-  license: packageJSON.license,
-  dependencies: packageJSON.dependencies,
-  peerDependencies: packageJSON.peerDependencies,
-  type: 'module',
-  files: ['*'],
-  sideEffects: false,
-  exports: fs.readdirSync(distFolder).map((dir) => ({
-    [`./${dir}`]: {
-      import: `./${dir}/index.mjs`,
-      require: `./${dir}/index.js`,
-      types: `./${dir}/index.d.ts`,
-    },
-  })),
-};
+function generateExports() {
+  const exports = {};
 
-fs.writeFileSync(newPackagePath, JSON.stringify(newPackageJSON, null, 2));
+  fs.readdirSync(distFolder).forEach((dir) => {
+    exports[`./${dir}`] = {
+      import: `./dist/${dir}/index.mjs`,
+      require: `./dist/${dir}/index.js`,
+      types: `./dist/${dir}/index.d.ts`,
+    };
+  });
 
-/* Copy static files */
-const staticFiles = ['CHANGELOG.md', 'README.md', 'yarn.lock'];
-staticFiles.forEach((file) => {
-  const source = path.resolve(file);
-  const target = path.resolve(`../dist/${file}`);
+  return exports;
+}
 
-  fs.copyFileSync(source, target);
-});
+packageJSON.exports = generateExports();
+
+fs.writeFileSync(packagePath, JSON.stringify(packageJSON, null, 2));
