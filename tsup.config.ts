7376d7caca0e64fd83bd7521e6d49b0e7758730b
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-02 15:16:35 -0400

    Bring script into file

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,24 +1,5 @@
 import { defineConfig } from 'tsup';
-import type { Plugin } from 'esbuild';
-
-const insertUseClient: Plugin = {
-  name: 'insert-use-client',
-  setup(build) {
-    build.onLoad({ filter: /\.js$/ }, async (args) => {
-      const fs = require('fs');
-      let contents = fs.readFileSync(args.path, 'utf8');
-      const useClientDirective = `'use client';`;
-
-      if (contents.includes(useClientDirective)) {
-        contents = contents.replace(useClientDirective, '').trim();
-        return {
-          contents: useClientDirective + '\n\n' + contents,
-          loader: 'default',
-        };
-      }
-    });
-  },
-};
+import { insertUseClient } from './scripts/insert-use-client';
 
 export default defineConfig({
   entry: ['src/**/*.ts', 'src/**/*.tsx', '!src/**/*.stories.tsx'],
