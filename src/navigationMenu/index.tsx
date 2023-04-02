93bd95adfdd6dc6bfcc6673b51e513484b33603e
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-02 11:48:11 -0400

    Fix Navigation Menu component

--- src/navigationMenu/index.tsx
+++ src/navigationMenu/index.tsx
@@ -1,31 +1,33 @@
 import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu';
 import clsx from 'clsx';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
-import type { ComponentPropsWithoutRef, ElementRef } from 'react';
+import type { ComponentPropsWithoutRef, ElementRef, FC } from 'react';
 import { forwardRef } from 'react';
+import Link from 'next/link';
 
-const NavigationMenuViewport = forwardRef<
-  ElementRef<typeof NavigationMenuPrimitive.Viewport>,
-  ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>
->(({ className, ...props }, ref) => (
-  <div className={clsx('absolute left-0 top-full flex justify-center')}>
-    <NavigationMenuPrimitive.Viewport
-      className={clsx(
-        'origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border border-neutral-200 bg-white shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 dark:border-neutral-700 dark:bg-neutral-800 md:w-[var(--radix-navigation-menu-viewport-width)]',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  </div>
-));
-NavigationMenuViewport.displayName =
-  NavigationMenuPrimitive.Viewport.displayName;
+type NavigationDropdownItemProps = {
+  layout: 'list' | 'grid';
+  items: ({
+    label: string;
+    description?: string;
+  } & ComponentPropsWithoutRef<typeof Link>)[];
+  feature?: FC;
+};
 
-const NavigationMenu = forwardRef<
+type NavigationLinkItemProps = ComponentPropsWithoutRef<typeof Link>;
+
+type NavigationMenuProps = ComponentPropsWithoutRef<
+  typeof NavigationMenuPrimitive.Root
+> & {
+  items: ({
+    label: string;
+  } & (NavigationDropdownItemProps | NavigationLinkItemProps))[];
+};
+
+export const NavigationMenu = forwardRef<
   ElementRef<typeof NavigationMenuPrimitive.Root>,
-  ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+  NavigationMenuProps
+>(({ className, children, items, ...props }, ref) => (
   <NavigationMenuPrimitive.Root
     ref={ref}
     className={clsx(
@@ -34,93 +36,76 @@ const NavigationMenu = forwardRef<
     )}
     {...props}
   >
-    {children}
-    <NavigationMenuViewport />
+    <NavigationMenuPrimitive.List className="group flex flex-1 list-none items-center justify-center">
+      {items.map((item) => (
+        <NavigationMenuPrimitive.Item key={item.label}>
+          {'href' in item ? (
+            <NavigationMenuPrimitive.Link
+              href={item.href as string}
+              asChild
+              className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-neutral-100 focus:bg-neutral-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-neutral-50 data-[state=open]:bg-neutral-50 dark:text-neutral-100 dark:hover:bg-neutral-800 dark:hover:text-neutral-100 dark:focus:bg-neutral-800 dark:data-[active]:bg-neutral-800 dark:data-[state=open]:bg-neutral-800"
+            >
+              <Link href={item.href}>{item.label}</Link>
+            </NavigationMenuPrimitive.Link>
+          ) : (
+            <>
+              <NavigationMenuPrimitive.Trigger className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-neutral-100 focus:bg-neutral-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-neutral-50 data-[state=open]:bg-neutral-50 dark:text-neutral-100 dark:hover:bg-neutral-800 dark:hover:text-neutral-100 dark:focus:bg-neutral-800 dark:data-[active]:bg-neutral-800 dark:data-[state=open]:bg-neutral-800">
+                {item.label}
+                <ChevronDownIcon
+                  className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
+                  aria-hidden="true"
+                />
+              </NavigationMenuPrimitive.Trigger>
+              <NavigationMenuPrimitive.Content className="left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto">
+                <ul
+                  className={clsx(
+                    'grid gap-3',
+                    item.layout === 'list' &&
+                      'w-[400px] p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]',
+                    item.layout === 'grid' &&
+                      'p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'
+                  )}
+                >
+                  {item.feature && (
+                    <li className="row-span-3">
+                      <item.feature />
+                    </li>
+                  )}
+                  {item.items?.map((subItem) => (
+                    <NavigationMenuPrimitive.Item>
+                      <NavigationMenuPrimitive.Link
+                        href={subItem.href as string}
+                        asChild
+                      >
+                        <Link
+                          href={subItem.href}
+                          className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-slate-100 focus:bg-slate-100 dark:hover:bg-slate-700 dark:focus:bg-slate-700"
+                        >
+                          <div className="text-sm font-medium leading-none">
+                            {subItem.label}
+                          </div>
+                          <p className="line-clamp-2 text-sm leading-snug text-slate-500 dark:text-slate-400">
+                            {subItem.description}
+                          </p>
+                        </Link>
+                      </NavigationMenuPrimitive.Link>
+                    </NavigationMenuPrimitive.Item>
+                  ))}
+                </ul>
+              </NavigationMenuPrimitive.Content>
+            </>
+          )}
+        </NavigationMenuPrimitive.Item>
+      ))}
+      {children}
+
+      {/* <NavigationMenuPrimitive.Indicator className="top-full z-[1] flex h-1.5 items-end justify-center overflow-hidden data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:fade-in">
+        <div className="relative top-[60%] h-2 w-2 rotate-45 rounded-tl-sm bg-neutral-200 shadow-md dark:bg-neutral-800" />
+      </NavigationMenuPrimitive.Indicator> */}
+    </NavigationMenuPrimitive.List>
+    <div className="absolute left-0 top-full flex justify-center">
+      <NavigationMenuPrimitive.Viewport className="origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border border-neutral-200 bg-white shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 dark:border-neutral-700 dark:bg-neutral-800 md:w-[var(--radix-navigation-menu-viewport-width)]" />
+    </div>
   </NavigationMenuPrimitive.Root>
 ));
 NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName;
-
-const NavigationMenuList = forwardRef<
-  ElementRef<typeof NavigationMenuPrimitive.List>,
-  ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.List>
->(({ className, ...props }, ref) => (
-  <NavigationMenuPrimitive.List
-    ref={ref}
-    className={clsx(
-      'group flex flex-1 list-none items-center justify-center',
-      className
-    )}
-    {...props}
-  />
-));
-NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
-
-const NavigationMenuItem = NavigationMenuPrimitive.Item;
-
-const NavigationMenuTrigger = forwardRef<
-  ElementRef<typeof NavigationMenuPrimitive.Trigger>,
-  ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <NavigationMenuPrimitive.Trigger
-    ref={ref}
-    className={clsx(
-      'group inline-flex h-10 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-neutral-100 focus:bg-neutral-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-neutral-50 data-[state=open]:bg-neutral-50 dark:text-neutral-100 dark:hover:bg-neutral-800 dark:hover:text-neutral-100 dark:focus:bg-neutral-800 dark:data-[active]:bg-neutral-800 dark:data-[state=open]:bg-neutral-800',
-      'group',
-      className
-    )}
-    {...props}
-  >
-    {children}{' '}
-    <ChevronDownIcon
-      className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
-      aria-hidden="true"
-    />
-  </NavigationMenuPrimitive.Trigger>
-));
-NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName;
-
-const NavigationMenuContent = forwardRef<
-  ElementRef<typeof NavigationMenuPrimitive.Content>,
-  ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <NavigationMenuPrimitive.Content
-    ref={ref}
-    className={clsx(
-      'left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto ',
-      className
-    )}
-    {...props}
-  />
-));
-NavigationMenuContent.displayName = NavigationMenuPrimitive.Content.displayName;
-
-const NavigationMenuLink = NavigationMenuPrimitive.Link;
-
-const NavigationMenuIndicator = forwardRef<
-  ElementRef<typeof NavigationMenuPrimitive.Indicator>,
-  ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Indicator>
->(({ className, ...props }, ref) => (
-  <NavigationMenuPrimitive.Indicator
-    ref={ref}
-    className={clsx(
-      'top-full z-[1] flex h-1.5 items-end justify-center overflow-hidden data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:fade-in',
-      className
-    )}
-    {...props}
-  >
-    <div className="relative top-[60%] h-2 w-2 rotate-45 rounded-tl-sm bg-neutral-200 shadow-md dark:bg-neutral-800" />
-  </NavigationMenuPrimitive.Indicator>
-));
-NavigationMenuIndicator.displayName =
-  NavigationMenuPrimitive.Indicator.displayName;
-
-export {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuContent,
-  NavigationMenuTrigger,
-  NavigationMenuLink,
-  NavigationMenuIndicator,
-  NavigationMenuViewport,
-};
--- src/navigationMenu/navigationMenu.stories.tsx
+++ src/navigationMenu/navigationMenu.stories.tsx
@@ -1,152 +1,107 @@
-import type { ComponentPropsWithoutRef, ElementRef } from 'react';
-import { forwardRef } from 'react';
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-  NavigationMenuContent,
-  NavigationMenuTrigger,
-  NavigationMenuLink,
-  /*
-   * NavigationMenuIndicator,
-   * NavigationMenuViewport,
-   */
-} from './';
+import { NavigationMenu } from './';
 import { CommandLineIcon } from '@heroicons/react/20/solid';
-import clsx from 'clsx';
 import Link from 'next/link';
 
 export default {
-  title: 'Example/Navigation Menu',
+  label: 'Example/Navigation Menu',
   component: NavigationMenu,
   parameters: {
     layout: 'fullscreen',
   },
 } as ComponentMeta<typeof NavigationMenu>;
 
-// eslint-disable-next-line react/display-name
-const ListItem = forwardRef<ElementRef<'a'>, ComponentPropsWithoutRef<'a'>>(
-  ({ className, title, children, ...props }, ref) => (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={clsx(
-            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700',
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-neutral-500 dark:text-neutral-400">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  )
-);
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Alert Dialog',
-    href: '/docs/primitives/alert-dialog',
-    description:
-      'A modal dialog that interrupts the user with important content and expects a response.',
-  },
-  {
-    title: 'Hover Card',
-    href: '/docs/primitives/hover-card',
-    description:
-      'For sighted users to preview content available behind a link.',
-  },
-  {
-    title: 'Progress',
-    href: '/docs/primitives/progress',
-    description:
-      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-  },
-  {
-    title: 'Scroll-area',
-    href: '/docs/primitives/scroll-area',
-    description: 'Visually or semantically separates content.',
-  },
-  {
-    title: 'Tabs',
-    href: '/docs/primitives/tabs',
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-  {
-    title: 'Tooltip',
-    href: '/docs/primitives/tooltip',
-    description:
-      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-  },
-];
-
 const Template: ComponentStory<typeof NavigationMenu> = (args) => (
-  <NavigationMenu {...args}>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-        <NavigationMenuContent>
-          <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-            <li className="row-span-3">
-              <NavigationMenuLink asChild>
-                <a
-                  className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-rose-500 to-indigo-700 p-6 no-underline outline-none focus:shadow-md"
-                  href="/"
-                >
-                  <CommandLineIcon className="h-6 w-6 text-white" />
-                  <div className="mb-2 mt-4 text-lg font-medium text-white">
-                    shadcn/ui
-                  </div>
-                  <p className="text-sm leading-tight text-white/90">
-                    Beautifully designed components built with Radix UI and
-                    Tailwind CSS.
-                  </p>
-                </a>
-              </NavigationMenuLink>
-            </li>
-            <ListItem href="/docs" title="Introduction">
-              Re-usable components built using Radix UI and Tailwind CSS.
-            </ListItem>
-            <ListItem href="/docs/installation" title="Installation">
-              How to install dependencies and structure your app.
-            </ListItem>
-            <ListItem href="/docs/primitives/typography" title="Typography">
-              Styles for headings, paragraphs, lists...etc
-            </ListItem>
-          </ul>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-        <NavigationMenuContent>
-          <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-            {components.map((component) => (
-              <ListItem
-                key={component.title}
-                title={component.title}
-                href={component.href}
-              >
-                {component.description}
-              </ListItem>
-            ))}
-          </ul>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <Link href="/docs" legacyBehavior passHref>
-          <NavigationMenuLink>Documentation</NavigationMenuLink>
-        </Link>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-  </NavigationMenu>
+  <NavigationMenu {...args} />
 );
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  items: [
+    {
+      label: 'Getting started',
+      layout: 'list',
+      items: [
+        {
+          label: 'Introduction',
+          href: '/docs',
+          description:
+            'Re-usable components built using Radix UI and Tailwind CSS.',
+        },
+        {
+          label: 'Installation',
+          href: '/docs/installation',
+          description: 'How to install dependencies and structure your app.',
+        },
+        {
+          label: 'Typography',
+          href: '/docs/primitives/typography',
+          description: 'Styles for headings, paragraphs, lists...etc',
+        },
+      ],
+      feature: () => (
+        <Link
+          className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-rose-500 to-indigo-700 p-6 no-underline outline-none focus:shadow-md"
+          href="/"
+        >
+          <CommandLineIcon className="h-6 w-6 text-white" />
+          <div className="mb-2 mt-4 text-lg font-medium text-white">
+            shadcn/ui
+          </div>
+          <p className="text-sm leading-tight text-white/90">
+            Beautifully designed components built with Radix UI and Tailwind
+            CSS.
+          </p>
+        </Link>
+      ),
+    },
+    {
+      label: 'Components',
+      layout: 'grid',
+      items: [
+        {
+          label: 'Alert Dialog',
+          href: '/docs/primitives/alert-dialog',
+          description:
+            'A modal dialog that interrupts the user with important content and expects a response.',
+        },
+        {
+          label: 'Hover Card',
+          href: '/docs/primitives/hover-card',
+          description:
+            'For sighted users to preview content available behind a link.',
+        },
+        {
+          label: 'Progress',
+          href: '/docs/primitives/progress',
+          description:
+            'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
+        },
+        {
+          label: 'Scroll-area',
+          href: '/docs/primitives/scroll-area',
+          description: 'Visually or semantically separates content.',
+        },
+        {
+          label: 'Tabs',
+          href: '/docs/primitives/tabs',
+          description:
+            'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
+        },
+        {
+          label: 'Tooltip',
+          href: '/docs/primitives/tooltip',
+          description:
+            'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+        },
+      ],
+    },
+    {
+      label: 'Documentation',
+      href: '/docs',
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    },
+  ],
+};
