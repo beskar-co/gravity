42a7ebc161c6f519109a5fda70598a50ec4a3f38
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-02 18:15:47 -0400

    Remove unused code

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,24 +1,4 @@
 import { defineConfig } from 'tsup';
-import type { Plugin } from 'esbuild';
-
-const insertUseClient: Plugin = {
-  name: 'insert-use-client',
-  setup(build) {
-    build.onLoad({ filter: /\.js$/ }, async (args) => {
-      const fs = require('fs');
-      let contents = fs.readFileSync(args.path, 'utf8');
-      const useClientDirective = `"use client";`;
-
-      if (contents.includes(useClientDirective)) {
-        contents = contents.replace(useClientDirective, '').trim();
-        return {
-          contents: useClientDirective + '\n\n' + contents,
-          loader: 'default',
-        };
-      }
-    });
-  },
-};
 
 export default defineConfig({
   entry: ['src/**/*.ts', 'src/**/*.tsx', '!src/**/*.stories.tsx'],
@@ -26,5 +6,4 @@ export default defineConfig({
   dts: true,
   format: ['cjs', 'esm'],
   splitting: false,
-  esbuildPlugins: [insertUseClient],
 });
