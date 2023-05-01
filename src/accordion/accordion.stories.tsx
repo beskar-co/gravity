e8a34b07c08e392cb99f0aca86a0a4409d3edae9
Hayden Bleasel <hello@haydenbleasel.com>
2023-04-30 22:11:22 -0400

    fix(accordion): rebuild accordion

--- src/accordion/accordion.stories.tsx
+++ src/accordion/accordion.stories.tsx
@@ -1,11 +1,6 @@
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from './';
+import { Accordion } from './';
 
 export default {
   title: 'Example/Accordion',
@@ -16,31 +11,30 @@ export default {
 } as ComponentMeta<typeof Accordion>;
 
 const Template: ComponentStory<typeof Accordion> = (args) => (
-  <Accordion {...args}>
-    <AccordionItem value="item-1">
-      <AccordionTrigger>Is it accessible?</AccordionTrigger>
-      <AccordionContent>
-        Yes. It adheres to the WAI-ARIA design pattern.
-      </AccordionContent>
-    </AccordionItem>
-    <AccordionItem value="item-2">
-      <AccordionTrigger>Is it styled?</AccordionTrigger>
-      <AccordionContent>
-        Yes. It comes with default styles that matches the other
-        components&apos; aesthetic.
-      </AccordionContent>
-    </AccordionItem>
-    <AccordionItem value="item-3">
-      <AccordionTrigger>Is it animated?</AccordionTrigger>
-      <AccordionContent>
-        Yes. It is animated by default, but you can disable it if you prefer.
-      </AccordionContent>
-    </AccordionItem>
-  </Accordion>
+  <Accordion {...args} />
 );
 
 export const Default = Template.bind({});
 Default.args = {
   type: 'single',
   collapsible: true,
+  data: [
+    {
+      value: 'item-1',
+      trigger: 'Is it accessible?',
+      content: 'Yes. It adheres to the WAI-ARIA design pattern.',
+    },
+    {
+      value: 'item-2',
+      trigger: 'Is it styled?',
+      content:
+        "Yes. It comes with default styles that matches the other components' aesthetic.",
+    },
+    {
+      value: 'item-3',
+      trigger: 'Is it animated?',
+      content:
+        'Yes. It is animated by default, but you can disable it if you prefer.',
+    },
+  ],
 };
--- src/accordion/index.tsx
+++ src/accordion/index.tsx
@@ -1,66 +1,53 @@
 'use client';
 
-import type { ElementRef, ComponentPropsWithoutRef } from 'react';
+import type { ElementRef, ComponentPropsWithoutRef, FC } from 'react';
 import { forwardRef } from 'react';
 import * as AccordionPrimitive from '@radix-ui/react-accordion';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
 import clsx from 'clsx';
 
-const Accordion = AccordionPrimitive.Root;
+type AccordionProps = ComponentPropsWithoutRef<
+  typeof AccordionPrimitive.Root
+> & {
+  data: {
+    value: string;
+    trigger: AccordionPrimitive.AccordionTriggerProps['children'];
+    content: AccordionPrimitive.AccordionContentProps['children'];
+  }[];
+};
 
-const AccordionItem = forwardRef<
-  ElementRef<typeof AccordionPrimitive.Item>,
-  ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
->(({ className, ...props }, ref) => (
-  <AccordionPrimitive.Item
-    ref={ref}
-    className={clsx(
-      'border-b',
-      'border-b-neutral-200 dark:border-b-neutral-700',
-      className
-    )}
-    {...props}
-  />
+export const Accordion = forwardRef<
+  ElementRef<typeof AccordionPrimitive.Root>,
+  AccordionProps
+>(({ className, data, ...props }, ref) => (
+  <AccordionPrimitive.Root ref={ref} className={className} {...props}>
+    {data.map(({ value, trigger, content }) => (
+      <AccordionPrimitive.Item
+        className="border-b border-b-neutral-200 dark:border-b-neutral-700"
+        value={value}
+        key={value}
+      >
+        <AccordionPrimitive.Header className="flex">
+          <AccordionPrimitive.Trigger
+            className={clsx(
+              'flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+              'text-neutral-900 dark:text-white'
+            )}
+          >
+            {trigger}
+            <ChevronDownIcon className="h-4 w-4 transition-transform duration-200" />
+          </AccordionPrimitive.Trigger>
+        </AccordionPrimitive.Header>
+        <AccordionPrimitive.Content
+          className={clsx(
+            'overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down',
+            'text-neutral-900 dark:text-white'
+          )}
+        >
+          <div className="pb-4 pt-0">{content}</div>
+        </AccordionPrimitive.Content>
+      </AccordionPrimitive.Item>
+    ))}
+  </AccordionPrimitive.Root>
 ));
-AccordionItem.displayName = 'AccordionItem';
-
-const AccordionTrigger = forwardRef<
-  ElementRef<typeof AccordionPrimitive.Trigger>,
-  ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-  <AccordionPrimitive.Header className="flex">
-    <AccordionPrimitive.Trigger
-      ref={ref}
-      className={clsx(
-        'flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
-        'text-neutral-900 dark:text-white',
-        className
-      )}
-      {...props}
-    >
-      {children}
-      <ChevronDownIcon className="h-4 w-4 transition-transform duration-200" />
-    </AccordionPrimitive.Trigger>
-  </AccordionPrimitive.Header>
-));
-AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
-
-const AccordionContent = forwardRef<
-  ElementRef<typeof AccordionPrimitive.Content>,
-  ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
->(({ className, children, ...props }, ref) => (
-  <AccordionPrimitive.Content
-    ref={ref}
-    className={clsx(
-      'overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down',
-      'text-neutral-900 dark:text-white',
-      className
-    )}
-    {...props}
-  >
-    <div className="pb-4 pt-0">{children}</div>
-  </AccordionPrimitive.Content>
-));
-AccordionContent.displayName = AccordionPrimitive.Content.displayName;
-
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
+Accordion.displayName = AccordionPrimitive.Root.displayName;
