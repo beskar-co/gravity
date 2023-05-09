b6bcde9a91ec7988353778884fa52925abbf662b
Hayden Bleasel <hello@haydenbleasel.com>
2023-05-09 13:41:49 -0400

    Run npx sb upgrade

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,33 +1,30 @@
 import type { StorybookViteConfig } from '@storybook/builder-vite';
-
 const config: StorybookViteConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        /*
-         * Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-         * For more details on this addon's options.
-         */
-        postCss: true,
-      },
-    },
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', {
+    name: '@storybook/addon-styling',
+    options: {
+      /*
+       * Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+       * For more details on this addon's options.
+       */
+      postCss: true
+    }
+  }, '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
   },
+  core: {},
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
   viteFinal(viteConfig) {
     // Add your configuration here
     return viteConfig;
   },
+  docs: {
+    autodocs: true
+  }
 };
-
 export default config;
