c1ae64ebbc43a64236b05a6f2a43b409c807ca05
Hayden Bleasel <hello@haydenbleasel.com>
2023-05-24 14:12:54 -0400

    fix(tooltip): also support align

--- src/tooltip/index.tsx
+++ src/tooltip/index.tsx
@@ -16,12 +16,13 @@ type TooltipProps = ComponentPropsWithoutRef<typeof TooltipPrimitive.Root> & {
   content: string;
   children: ReactNode;
   side?: TooltipPrimitive.TooltipContentProps['side'];
+  align?: TooltipPrimitive.TooltipContentProps['align'];
 };
 
 const Tooltip: FC<TooltipProps> = forwardRef<
   ElementRef<typeof TooltipPrimitive.Root>,
   TooltipProps
->(({ children, content, side, ...props }, ref) => (
+>(({ children, content, side, align, ...props }, ref) => (
   <TooltipPrimitive.Root delayDuration={0} {...props}>
     <TooltipPrimitive.Trigger ref={ref} asChild>
       {children}
@@ -29,6 +30,7 @@ const Tooltip: FC<TooltipProps> = forwardRef<
     <TooltipPrimitive.Content
       sideOffset={4}
       side={side}
+      align={align}
       className={clsx(
         'z-50 overflow-hidden rounded-md border border-neutral-100 bg-white px-3 py-1.5 text-sm text-neutral-700 shadow-md animate-in fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1 dark:border-neutral-800 dark:bg-neutral-800 dark:text-neutral-400'
       )}
